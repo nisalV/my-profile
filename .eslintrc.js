017d110554e52d9584c9f8b935619f7c68fbc9cd
Nisal Perera <nisal.pathmila.perera@gmail.com>
2024-02-29 16:36:43 +0530

    Setup with webpack & typescript

---
+++ .eslintrc.js
@@ -0,0 +1,35 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
+    'plugin:prettier/recommended',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['react', '@typescript-eslint'],
+  rules: {
+    '@typescript-eslint/no-empty-function': 'off',
+    '@typescript-eslint/no-unused-vars': ['error'],
+    '@typescript-eslint/no-var-requires': 'off',
+    'react/prop-types': 'off',
+    'react/jsx-uses-react': 'off',
+    'react/react-in-jsx-scope': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+}
---
+++ webpack.config.js
@@ -0,0 +1,82 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const Dotenv = require('dotenv-webpack')
+
+module.exports = (env) => {
+  const mode = env.env == 'prod' ? 'production' : 'development'
+
+  const isSecure = env.mode === 'secure'
+
+  const config = {
+    entry: path.resolve(__dirname, 'src', 'index.tsx'),
+    output: {
+      path: path.resolve(__dirname, 'build'),
+      filename: '[name].js',
+      publicPath: '/',
+    },
+    mode: mode,
+    module: {
+      rules: [
+        {
+          test: /\.(js|jsx)$/,
+          use: ['babel-loader'],
+        },
+        {
+          test: /\.(ts|tsx)$/,
+          include: [path.resolve(__dirname, 'src')],
+          use: ['ts-loader'],
+        },
+        {
+          test: /\.css$/i,
+          use: ['style-loader', 'css-loader'],
+        },
+        {
+          test: /\.(?:ico)$/i,
+          type: 'asset/resource',
+        },
+        {
+          test: /\.(gif|jpe?g|png|svg)$/,
+          use: {
+            loader: 'url-loader',
+            options: {
+              name: '[name].[ext]',
+              esModule: false,
+            },
+          },
+        },
+      ],
+    },
+    resolve: {
+      extensions: ['.js', '.jsx', '.ts', '.tsx'],
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: 'public/index.html',
+        favicon: 'public/favicon.ico',
+        manifest: 'public/manifest.json',
+      }),
+      new Dotenv({
+        path: `./env/${env.env}.env`,
+      }),
+    ],
+    devServer: {
+      historyApiFallback: true,
+      hot: true,
+      open: true,
+      compress: true,
+      port: 5000,
+    },
+  }
+
+  if (isSecure) {
+    config.devServer['server'] = {
+      type: 'https',
+      options: {
+        key: './ssl/dev.local.key',
+        cert: './ssl/dev.local.crt',
+      },
+    }
+  }
+
+  return config
+}
